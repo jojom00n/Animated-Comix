2c4dbc3c81e053fc61d67a3f9c2105a62b3afdc9
Andrew <linkegamer@gmail.com>
2024-05-11 18:48:34 +0300

    интерфейс 11.05

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide6.QtSql import QSqlTableModel
 
-from ui_main3 import Ui_MainWindow
-# from ui_main3 import MouseTracker
+from ui_main import Ui_MainWindow
+# from ui_main import MouseTracker
 
 from PySide6 import QtCore, QtGui
 from PySide6.QtCore import Qt, QPointF
