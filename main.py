38312b254af7dc258768848ed28302729e39eb65
BuildTools <unconfigured@null.spigotmc.org>
2024-05-23 13:40:35 +0300

    Подключили к основной ветке uimain

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sys
+
+from PySide6 import QtCore
+from PySide6.QtWidgets import QMainWindow, QApplication
+
 from htmlparcing import *
 
 from ComixProject import *
+from ui_main import Ui_MainWindow
 
 f_css = """
 
@@ -292,10 +298,35 @@ f_html = """
 </body></html>
 """
 
-parcer = htmlparcing()
-parcer.read_html(f_html2)
-parcer.read_css(f_css)
-project = ComixProject()
-com = parcer.setup_com_project(project)
-print(com)
+class Comix(QMainWindow):
+    def __init__(self): # Метод инициализации окна
+        super(Comix, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Создаем экземпляр класса MouseTracker с передачей метки в качестве аргумента
+        # self.tracker = MouseTracker(self.ui.main_work_space, self.ui.label_position)
+        # self.tracker.positionChanged.connect(self.on_positionChanged)
+
+
+    # Метод вывода в правом нижнем углу экрана координат курсора
+    @QtCore.Slot(QtCore.QPoint)
+    def on_positionChanged(self, pos):
+        # Меняем текст метки с текущими координатами курсора
+        self.ui.label_position.setText("X: %d, Y: %d" % (pos.x(), pos.y()))
+        self.ui.label_position.adjustSize()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Comix()
+    window.show()
+    parcer = htmlparcing()
+    parcer.read_html(f_html2)
+    parcer.read_css(f_css)
+    project = ComixProject()
+    com = parcer.setup_com_project(project)
+    print(project)
+    sys.exit(app.exec())
+
+
 
